996bf1ec5dc3d7baeca26e5c86047d73b72e4aa5
spalpeen <oxybimboo@gmail.com>
2022-05-24 12:38:22 +0200

    Lesson 3. Complete homework.

---
+++ todoproject/views.py
@@ -0,0 +1,13 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Project, ToDo
+from .serializers import ProjectSerializer, ToDoSerializer
+
+
+class ProjectModelViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class ToDoModelViewSet(ModelViewSet):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
