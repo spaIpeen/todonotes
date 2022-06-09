8f024183110c42198db3a94ca32ace4011e0e866
spaIpeen <84104057+spaIpeen@users.noreply.github.com>
2022-06-09 12:44:56 +0200

    Lesson 6. Complete homework. (#6)

--- todonotes/urls.py
+++ todonotes/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from users.views import ArticleCustomViewSet
 from todoproject.views import ProjectModelViewSet, ToDoModelViewSet
 
@@ -28,4 +33,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
