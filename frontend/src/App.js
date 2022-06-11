049d2900fcffab521128dd057a8e17db01bfc100
spalpeen <oxybimboo@gmail.com>
2022-06-11 13:44:10 +0200

    Lesson 7. Complete homework.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from "axios";
+import Cookies from 'universal-cookie';
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import './logo.svg';
 import './App.css';
@@ -8,6 +9,7 @@ import ProjectList from './components/Projects.js'
 import TodoList from './components/Todoes.js'
 import MenuItem from "./components/Menu";
 import FooterItem from "./components/Footer";
+import LoginForm from "./components/Auth";
 
 
 class App extends React.Component {
@@ -21,37 +23,78 @@ class App extends React.Component {
         }
     }
 
+    set_token(token) {
+        const cookies = new Cookies()
+        cookies.set('token', token)
+        this.setState({'token': token}, ()=>this.load_data())
+    }
+
+    is_authenticated() {
+        return this.state.token != ''
+    }
+
+    logout() {
+        this.set_token('')
+    }
+
+    get_token_from_storage() {
+        const cookies = new Cookies()
+        const token = cookies.get('token')
+        this.setState({'token': token}, ()=>this.load_data())
+    }
+
+    get_token(username, password) {
+        axios.post('http://127.0.0.1:8000/api-token-auth/', {username: username, password: password})
+        .then(response => {
+            this.set_token(response.data['token'])
+        }).catch(error => alert('Неверный логин или пароль'))
+    }
+
+    get_headers() {
+        let headers = {
+            'Content-Type': 'application/json'
+        }
+        if (this.is_authenticated()) {
+            headers['Authorization'] = 'Token ' + this.state.token
+        }
+        return headers
+    }
+
+    load_data() {
+        const headers = this.get_headers()
+        axios.get('http://127.0.0.1:8000/api/users', {headers})
+            .then(response => {
+                this.setState(
+                    {
+                        'users': response.data.results
+                    })
+            }).catch(error => console.log(error))
+
+        axios.get('http://127.0.0.1:8000/api/project', {headers})
+            .then(response => {
+                this.setState(
+                    {
+                        'projects': response.data.results
+                    })
+            }).catch(error => console.log(error))
+
+        axios.get('http://127.0.0.1:8000/api/todo', {headers})
+            .then(response => {
+                this.setState(
+                    {
+                        'todoes': response.data.results
+                    })
+            }).catch(error => {
+                    console.log(error)
+                    this.setState({'todoes': []})
+                })
+    }
+
     componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/users')
-            .then(response => {
-                const users = response.data.results
-                this.setState(
-                    {
-                        'users': users
-                    })
-            }).catch(error => console.log(error))
-
-        axios.get('http://127.0.0.1:8000/api/project')
-            .then(response => {
-                const projects = response.data.results
-                this.setState(
-                    {
-                        'projects': projects
-                    })
-            }).catch(error => console.log(error))
-
-        axios.get('http://127.0.0.1:8000/api/todo')
-            .then(response => {
-                const todoes = response.data.results
-                this.setState(
-                    {
-                        'todoes': todoes
-                    })
-            }).catch(error => console.log(error))
+        this.get_token_from_storage()
     }
 
     render () {
-        console.log('hello')
         return (
             <div>
                 <MenuItem/>
@@ -68,12 +111,18 @@ class App extends React.Component {
                                 <li>
                                     <Link to='/todoes'>Todoes</Link>
                                 </li>
+                                <li>
+                                    {this.is_authenticated() ? <button onClick={()=>this.logout()}>Logout</button> :
+                                    <Link to='/login'>Login</Link>}
+                                </li>
                             </ul>
                         </nav>
                         <Routes>
                             <Route path='/users' element={<UserList users={this.state.users} />}/>
-                            <Route path='/projects/*' element={<ProjectList projects={this.state.projects} />}/>
+                            <Route path='/projects' element={<ProjectList projects={this.state.projects} />}/>
                             <Route path='/todoes' element={<TodoList todoes={this.state.todoes} />}/>
+                            <Route path='/login' element={<LoginForm
+                                get_token={(username, password) => this.get_token(username, password)}/>}/>
                         </Routes>
                     </BrowserRouter>
                 </div>
