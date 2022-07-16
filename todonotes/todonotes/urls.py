82cf9ec0f75ac1eec2ec889e638427d9c7239834
spaIpeen <84104057+spaIpeen@users.noreply.github.com>
2022-07-16 14:36:21 +0200

    Lesson 12. Complete homework. (#12)

--- todonotes/todonotes/urls.py
+++ todonotes/todonotes/urls.py
@@ -24,7 +24,7 @@ from rest_framework_simplejwt.views import (
 )
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from graphene_django.views import GraphQLView
+#from graphene_django.views import GraphQLView
 
 from users.views import UserCustomViewSet
 from todoproject.views import ProjectModelViewSet, ToDoModelViewSet
@@ -57,5 +57,5 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('graphql/', GraphQLView.as_view(graphiql=True)),
+    #path('graphql/', GraphQLView.as_view(graphiql=True)),
 ]
