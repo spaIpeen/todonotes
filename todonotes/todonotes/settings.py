8f8bfe60b7205a8e04b5b8ddbd536443da6af5c3
spalpeen <oxybimboo@gmail.com>
2022-07-16 14:34:54 +0200

    Lesson 12. Complete homework.

--- todonotes/todonotes/settings.py
+++ todonotes/todonotes/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'drf_yasg',
-    'graphene_django',
+    #'graphene_django',
 
     'users',
     'todoproject'
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'todonotes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todonotesdb',
+        'USER': 'todonotes',
+        'PASSWORD': 'todo123456',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
